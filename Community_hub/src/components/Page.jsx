673147c95a4312182a81715d4edb72eb0e0bd9a1
JJ-Sof <jjose4@student.gsu.edu>
2024-03-02 11:58:07 -0500

    Updated attraction and Page jsx

---
+++ Community_hub/src/components/Page.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Page = () => {
+    return (
+        <div className="Calendar">
+            <table>
+                <thead>
+                    <tr>
+                        <th className="day">Day</th>
+                        <th>Sunday</th>
+                        <th>Monday</th>
+                        <th>Tuesday</th>
+                        <th>Wednesday</th>
+                        <th>Thursday</th>
+                        <th>Friday</th>
+                        <th>Saturday</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="time">8 AM</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>   
+        </div>
+    )
+}
+
+export default Calendar;
