a4ecbb3504344d4f56d92ce162f7a70127feea14
JJ-Sof <jjose4@student.gsu.edu>
2024-03-02 20:36:16 -0500

    aquarium and other pictures added

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 import Attraction from "./Attraction";
 
+import ParkImage from '../assets/Park.jpg';
+import ColaImage from '../assets/CocaCola.jpg';
+import CNNImage from '../assets/CNN.jpg';
+import MuseumImage from '../assets/Museum.jpg';
+import GardenImage from '../assets/Garden.jpg';
+import ZooImage from '../assets/Zoo.jpg';
+import HistoryCenterImage from '../assets/HistoryCenter.jpg';
+import FBankImage from '../assets/FBank.jpg';
+
+
 // Example list of attractions
 const attractions = [
-    { id: 1, name: 'Centennial Park', color: 'green', image: '/path/to/centennial-park-image.jpg'},
-    { id: 2, name: 'Georgia Aquarium', color: 'blue', image: '/path/to/centennial-park-image.jpg' },
-    { id: 3, name: 'World of Coca-Cola', color: 'red', image: '/path/to/centennial-park-image.jpg' },
-    { id: 3, name: 'CNN', color: 'black', image: '/path/to/centennial-park-image.jpg' },
-    { id: 3, name: 'MLK Museum', color: 'yellow', image: '/path/to/centennial-park-image.jpg' },
-    { id: 3, name: 'Atlanta Botanical Garden', color: 'white', image: '/path/to/centennial-park-image.jpg' },
-    { id: 3, name: 'Atlanta Zoo', color: 'orange', image: '/path/to/centennial-park-image.jpg' },
-    { id: 3, name: 'Atlanta History Center', color: 'grey', image: '/path/to/centennial-park-image.jpg' },
-    { id: 3, name: 'Fernbank Museum', color: 'brown', image: '/path/to/centennial-park-image.jpg' }
+    { id: 1, name: 'Centennial Park', color: 'green', image: ParkImage },
+    { id: 2, name: 'Georgia Aquarium', color: 'blue', image: '/Aquarium.jpg' },
+    { id: 3, name: 'World of Coca-Cola', color: 'red', image: ColaImage },
+    { id: 4, name: 'CNN Studio', color: 'violet', image: CNNImage },
+    { id: 5, name: 'Atlanta Civil Rights Museum', color: 'yellow', image: MuseumImage },
+    { id: 6, name: 'Atlanta Botanical Garden', color: 'white', image: GardenImage },
+    { id: 7, name: 'Atlanta Zoo', color: 'orange', image: ZooImage },
+    { id: 8, name: 'Atlanta History Center', color: 'grey', image: HistoryCenterImage },
+    { id: 9, name: 'Fernbank Museum', color: 'brown', image: FBankImage }
   
 ];
 
